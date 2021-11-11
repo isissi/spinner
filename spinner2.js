d23daafd14b6e650c6a1ad490f0442444eb446ad
isissi <isissi_525@hotmail.com>
2021-11-11 11:53:59 -0700

    update

--- spinner2.js
+++ spinner2.js
@@ -2,13 +2,13 @@ process.stdout.write('hello form spinner1.js... \rheyyy\n');
 
 let count = 0;
 let time = 100;
+const symboles = '|/-\\';
 
 while (count < 10) {
-  const symboles = "|/-\";
   for (let symbole of symboles) {
     setTimeout(() => {
       process.stdout.write(`\r${symbole}   `);
-    }, 100);
+    }, time);
     time += 200;
     count += 1;
   }
